885b697ad83654ac3d25fb9b0bf65e4bea6e5ef2
cskrada <frada@overcom.net>
2018-04-10 13:38:31 -0400

    se agregaron los metodos (index, registrar, actualizar, desactivar, activar)de mi clase categoria donde tambien se añadieron las rutas comrrespondientes

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,18 +14,34 @@ class CategoryController extends Controller
     }
 
     public function store (Request $request){
-
+    	$category = new Category();
+    	$category->name = $request->name;
+    	$category->description = $request->description;
+    	$category->condition = '1';
+    	$category->save();
     }
 
     public function update (Request $request){
+    	//realiza una comprobacion antes de salvar los datos mediante field o find or fail
+
+    	//el objeto va a ser igual a la clase categoria
+    	$category = Category::findOrFail($request->id);
+    	$category->name = $request->name;
+    	$category->description = $request->description;
+    	$category->condition = '1';
+    	$category->save();
 
     }
 
     public function desactivar (Request $request){
-
+    	$category = Category::findOrFail($request->id);
+		$category->condition = '0';
+		$category->save();
     }
 
     public function activar (Request $request){
-
+    	$category = Category::findOrFail($request->id);
+		$category->condition = '1';
+		$category->save();
     }
 }
