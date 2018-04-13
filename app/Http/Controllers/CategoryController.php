ecafa47e8966527cc874c6d68613b46cde45c1db
cskrada <frada@overcom.net>
2018-04-13 11:20:44 -0400

    se agregaron los protocolos de seguridad a nuestro sistema

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,12 +9,14 @@ class CategoryController extends Controller
 {
     //
     public function index (Request $request){
+        if (!$request->ajax()) return redirect('/');
     	$categories = Category::all();
     	return $categories;
     }
 
     public function store (Request $request){
-    	$category = new Category();
+    	if (!$request->ajax()) return redirect('/');
+        $category = new Category();
     	$category->name = $request->name;
     	$category->description = $request->description;
     	$category->condition = '1';
@@ -23,24 +25,25 @@ class CategoryController extends Controller
 
     public function update (Request $request){
     	//realiza una comprobacion antes de salvar los datos mediante field o find or fail
-
     	//el objeto va a ser igual a la clase categoria
-    	$category = Category::findOrFail($request->id);
+    	if (!$request->ajax()) return redirect('/');
+        $category = Category::findOrFail($request->id);
     	$category->name = $request->name;
     	$category->description = $request->description;
     	$category->condition = '1';
     	$category->save();
-
     }
 
     public function desactivar (Request $request){
-    	$category = Category::findOrFail($request->id);
+    	if (!$request->ajax()) return redirect('/');
+        $category = Category::findOrFail($request->id);
 		$category->condition = '0';
 		$category->save();
     }
 
     public function activar (Request $request){
-    	$category = Category::findOrFail($request->id);
+    	if (!$request->ajax()) return redirect('/');
+        $category = Category::findOrFail($request->id);
 		$category->condition = '1';
 		$category->save();
     }
