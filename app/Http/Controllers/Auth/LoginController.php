b3d0d1d7b98d2b4884cc66e4224e05bfceb752eb
cskrada <frada@overcom.net>
2018-04-25 11:21:27 -0400

    fue modificada la vista para aplicar csrf_field() para proteger el sistema contra ataques entre otras modificaciones con el mismo fin. en Auth.php se modifico el mensaje de error mientras que en el controlador se modificaron los metodos de Login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -15,15 +15,23 @@ class LoginController extends Controller
 
     //valida la peticion de inicio del usuario
     public function login(Request $request){
-        $this->validate($request, [
-            'user'     => 'required|string',
-            'password' => 'required|string'
-        ]);
+        $this->validateLogin($request);
+        
         // verifica si el usuario ingreso correctamente el usuario y la contraseña. Verifica la condicion del usuario es 1 para que pueda acceder
         if (Auth::attempt(['user' => $request->user,'password' => $request->password,'condition'=>1])){
             return redirect()->route('main');
         }
 
-        return back();
+        //con el metodo withErrors sirve para mostrar los errores pasando los parametros en el cual queremos reflejar en el identificador que en este caso es 'user' donde el mensaje que va a mostrar esta dentro de la carpeta resources/lang/en/auth.php
+        return back()
+        ->withErrors(['user'=> trans('auth.failed')])
+        ->withInput(request(['user']));
+    }
+
+    protected function validateLogin(Request $request){
+        $this->validate($request, [
+            'user'     => 'required|string',
+            'password' => 'required|string'
+        ]);
     }
 }
